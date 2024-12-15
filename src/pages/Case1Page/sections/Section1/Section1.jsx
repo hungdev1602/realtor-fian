702f532e6b339f2594930fd645faf5949e070cca
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-12-15 11:45:26 +0300

    responsive almost done page case1

--- src/pages/Case1Page/sections/Section1/Section1.jsx
+++ src/pages/Case1Page/sections/Section1/Section1.jsx
@@ -34,79 +34,142 @@ const Section1 = () => {
   ]
   return (
     <>
-      <div className="mt-[120px]">
+      <div className="mt-[80px] sm:mt-[120px]">
         <Title title={'Запрос клиента'} />
 
-        <div className="flex gap-[63px]">
+        <div className="flex flex-wrap xl:flex-nowrap gap-[30px] 2xl:gap-[63px]">
           {/* Left */}
-          <div className="w-[550px] h-[603px] truncate rounded-[20px]">
-            <img src={case1} alt="" className="w-full h-full object-cover"/>
+          <div className="w-full xl:w-[339px] 2xl:w-[550px] h-auto xl:h-[428px] 2xl:h-[603px] truncate rounded-none sm:rounded-[20px]">
+            <div className="block sm:hidden w-[951px] text-[11px] 2xl:text-[32px] font-[400] mt-0 2xl:mt-[55px] leading-[14px]">По запрошенным критериям помещения на рынке в столице не<br /> нашлось и клиент принял решение купить помещение.</div>
+            <img src={case1} alt="" className="hidden sm:block w-full h-full object-cover"/>
           </div>
 
           {/* Right */}
-          <div className="">
+          <div className="w-full lg:w-[832px] 2xl:w-auto">
+            {/* On PC */}
             {/* 1 row */}
-            <div className="flex items-center gap-[10px]">
+            <div className="hidden sm:flex items-center gap-[10px]">
               {/* 1 item */}
               <div 
-                className={"w-[200px] h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+                className={"w-[165px] 2xl:w-[200px] h-[110px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
               >
-                <span className="text-[20px] font-[900] absolute top-[15px] left-[15px]">1</span>
-                <div className="text-[18px] font-[500] text-center whitespace-pre-line ">{text[0].title}</div>
+                <span className="text-[10px] md:text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">1</span>
+                <div className="text-[12px] md:text-[18px] font-[500] text-center whitespace-pre-line ">{text[0].title}</div>
               </div>
               {/* 2 item */}
               <div 
-                className={"w-[380px] h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+                className={"w-[331px] 2xl:w-[380px] h-[110px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
               >
-                <span className="text-[20px] font-[900] absolute top-[15px] left-[15px]">2</span>
-                <div className="text-[18px] font-[500] text-center whitespace-pre-line ">{text[1].title}</div>
+                <span className="text-[10px] md:text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">2</span>
+                <div className="text-[12px] md:text-[18px] font-[500] text-center whitespace-pre-line ">{text[1].title}</div>
               </div>
               {/* 3 item */}
               <div 
-                className={"w-[407px] h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+                className={"w-[316px] 2xl:w-[407px] h-[110px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
               >
-                <span className="text-[20px] font-[900] absolute top-[15px] left-[15px]">3</span>
-                <div className="text-[18px] font-[500] text-center whitespace-pre-line ">{text[2].title}</div>
+                <span className="text-[10px] md:text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">3</span>
+                <div className="text-[12px] md:text-[18px] font-[500] text-center whitespace-pre-line ">{text[2].title}</div>
               </div>
             </div>
             {/* 2 row */}
-            <div className="flex items-center gap-[10px] mt-[10px]">
+            <div className="hidden sm:flex items-center gap-[10px] mt-[10px]">
               {/* 1 item */}
               <div 
-                className={"w-[251px] h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+                className={"w-[213px] 2xl:w-[251px] h-[110px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
               >
-                <span className="text-[20px] font-[900] absolute top-[15px] left-[15px]">4</span>
-                <div className="text-[18px] font-[500] text-center whitespace-pre-line ">{text[3].title}</div>
+                <span className="text-[10px] md:text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">4</span>
+                <div className="text-[12px] md:text-[18px] font-[500] text-center whitespace-pre-line ">{text[3].title}</div>
               </div>
               {/* 2 item */}
               <div 
-                className={"w-[156px] h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+                className={"w-[140px] 2xl:w-[156px] h-[110px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
               >
-                <span className="text-[20px] font-[900] absolute top-[15px] left-[15px]">5</span>
-                <div className="text-[18px] font-[500] text-center whitespace-pre-line ">{text[4].title}</div>
+                <span className="text-[10px] md:text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">5</span>
+                <div className="text-[12px] md:text-[18px] font-[500] text-center whitespace-pre-line ">{text[4].title}</div>
               </div>
               {/* 3 item */}
               <div 
-                className={"w-[300px] h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+                className={"w-[225px] 2xl:w-[300px] h-[110px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
               >
-                <span className="text-[20px] font-[900] absolute top-[15px] left-[15px]">6</span>
-                <div className="text-[18px] font-[500] text-center whitespace-pre-line ">{text[5].title}</div>
+                <span className="text-[10px] md:text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">6</span>
+                <div className="text-[12px] md:text-[18px] font-[500] text-center whitespace-pre-line ">{text[5].title}</div>
               </div>
               {/* 4 item */}
               <div 
-                className={"w-[270px] h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+                className={"w-[224px] 2xl:w-[270px] h-[110px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
               >
-                <span className="text-[20px] font-[900] absolute top-[15px] left-[15px]">7</span>
-                <div className="text-[18px] font-[500] text-center whitespace-pre-line ">{text[6].title}</div>
+                <span className="text-[10px] md:text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">7</span>
+                <div className="text-[12px] md:text-[18px] font-[500] text-center whitespace-pre-line ">{text[6].title}</div>
               </div>
             </div>
 
-            <div className="w-[951px] text-[32px] font-[400] mt-[55px]">По запрошенным критериям помещения на рынке в столице не нашлось и клиент принял решение <b>купить помещение.</b> </div>
+            {/* On Mobile */}
+            {/* 1 row */}
+            <div className="flex sm:hidden items-center gap-[10px]">
+              {/* 1 item */}
+              <div 
+                className={"w-[120px] 2xl:w-[200px] h-[90px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+              >
+                <span className="text-[10px] md:text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">1</span>
+                <div className="text-[12px] md:text-[18px] font-[500] text-center whitespace-pre-line ">{text[0].title}</div>
+              </div>
+              {/* 2 item */}
+              <div 
+                className={"w-[250px] 2xl:w-[380px] h-[90px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+              >
+                <span className="text-[10px] md:text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">2</span>
+                <div className="text-[12px] md:text-[18px] font-[500] text-center whitespace-pre-line ">{text[1].title}</div>
+              </div>
+            </div>
+            {/* 2 row */}
+            <div className="flex sm:hidden items-center gap-[10px] mt-[10px]">
+              {/* 1 item */}
+              <div 
+                className={"w-[185px] 2xl:w-[251px] h-[90px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+              >
+                <span className="text-[10px] md:text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">3</span>
+                <div className="text-[12px] md:text-[18px] font-[500] text-center whitespace-pre-line ">{text[2].title}</div>
+              </div>
+              {/* 2 item */}
+              <div 
+                className={"w-[185px] 2xl:w-[156px] h-[90px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+              >
+                <span className="text-[10px] md:text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">4</span>
+                <div className="text-[12px] md:text-[18px] font-[500] text-center whitespace-pre-line ">{text[3].title}</div>
+              </div>
+            </div>
+            {/* 3 row */}
+            <div className="flex sm:hidden items-center gap-[10px] mt-[10px]">
+              {/* 1 item */}
+              <div 
+                className={"w-[98px] 2xl:w-[251px] h-[90px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+              >
+                <span className="text-[10px] md:text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">5</span>
+                <div className="text-[12px] md:text-[18px] font-[500] text-center whitespace-pre-line ">{text[4].title}</div>
+              </div>
+              {/* 2 item */}
+              <div 
+                className={"w-[114px] 2xl:w-[156px] h-[90px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+              >
+                <span className="text-[10px] md:text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">6</span>
+                <div className="text-[12px] md:text-[18px] font-[500] text-center whitespace-pre-line ">{text[5].title}</div>
+              </div>
+              {/* 3 item */}
+              <div 
+                className={"w-[148px] 2xl:w-[300px] h-[90px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+              >
+                <span className="text-[10px] md:text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">7</span>
+                <div className="text-[12px] md:text-[18px] font-[500] text-center whitespace-pre-line ">{text[6].title}</div>
+              </div>
+            </div>
 
-            <div className="flex items-center gap-[111px] mt-[67px]">
+
+            <div className="hidden md:block w-[951px] text-[24px] 2xl:text-[32px] font-[400] mt-[30px] 2xl:mt-[55px] leading-[29px]">По запрошенным критериям помещения на рынке в столице <br /> не нашлось и клиент принял решение <b>купить помещение.</b> </div>
+
+            <div className="hidden sm:flex items-center gap-[111px] mt-[40px] 2xl:mt-[67px]">
               {/* Left */}
               <div className="flex items-center gap-[20px]">
-                <div className="w-[75px] h-[75px] truncate rounded-full">
+                <div className="w-[70px] 2xl:w-[75px] h-[70px] 2xl:h-[75px] truncate rounded-full">
                   <img src={case1_2} alt="" />
                 </div>
                 <div className="">
@@ -115,7 +178,7 @@ const Section1 = () => {
                 </div>
               </div>
               {/* Right */}
-              <button className="w-[350px] h-[75px] inline-flex items-center justify-center bg-[#333] text-[#fff] text-[24px] rounded-[20px]">Консультация</button>
+              <button className="w-[290px] 2xl:w-[350px] h-[70px] 2xl:h-[75px] inline-flex items-center justify-center bg-[#333] text-[#fff] text-[20px] 2xl:text-[24px] rounded-[20px]">Консультация</button>
             </div>
           </div>
         </div>
--- src/pages/Case1Page/sections/Section2/Section2.jsx
+++ src/pages/Case1Page/sections/Section2/Section2.jsx
@@ -7,11 +7,11 @@ const Section2 = () => {
       за рубежом `
     },
     {
-      title: `технический анализ и 
+      title: `тех. анализ и 
       заключение`
     },
     {
-      title: `юридическая проверка 
+      title: `юр. проверка 
       продавца и помещения`
     },
     {
@@ -25,68 +25,113 @@ const Section2 = () => {
   ]
   return (
     <>
-      <div className="mt-[120px]">
+      <div className="mt-[80px] sm:mt-[120px]">
         <Title title={'Решение'}/>
 
-        <div className="flex gap-[180px]">
+        <div className="flex flex-wrap xl:flex-nowrap gap-[15px] xl:gap-[30px] 2xl:gap-[180px]">
           {/* Left */}
 
-          <div className="text-[#fff]">
-            <div className="w-[694px] py-[24px] bg-[#A2999E] text-[28px] font-[500] text-center rounded-[20px]">Определили бюджет 100.000.000 руб. </div>
-            <div className="w-[694px] py-[24px] bg-[#A2999E] text-[28px] font-[500] text-center rounded-[20px] mt-[20px]">Нашли единственное отвечающее запросу: Подобные помещения стоили от 130.000.000 руб. </div>
+          <div className="w-full xl:w-auto text-[#fff]">
+            <div className="w-full xl:w-[615px] 2xl:w-[694px] py-[24px] bg-[#8F4449] text-[14px] sm:text-[20px] 2xl:text-[28px] font-[500] text-center rounded-[10px] sm:rounded-[20px]">Определили бюджет 100.000.000 руб. </div>
+            <div className="w-full xl:w-[615px] 2xl:w-[694px] py-[24px] bg-[#8F4449] text-[14px] sm:text-[20px] 2xl:text-[28px] font-[500] text-center rounded-[10px] sm:rounded-[20px] mt-[10px] sm:mt-[20px]">Нашли единственное отвечающее запросу: <br />Подобные помещения стоили от 130.000.000 руб. </div>
           </div>
 
           {/* Right */}
-          <div className="w-[746px] h-[695px] truncate rounded-[20px]">
+          <div className="w-full xl:w-[555px] 2xl:w-[746px] h-[270px] sm:h-[558px] 2xl:h-[695px] truncate rounded-[20px]">
             <img src={case1} alt="" className="w-full h-full object-cover"/>
           </div>
         </div>
 
-        <div className="mt-[-450px]">
+        <div className="mt-[20px] xl:mt-[-343px] 2xl:mt-[-450px]">
           <Title title={'Сложности'} />
 
           <div className="">
+            {/* On PC */}
             {/* 1 row */}
-            <div className="flex items-center gap-[10px]">
+            <div className="hidden sm:flex items-center gap-[10px]">
               {/* 1 item */}
               <div 
-                className={"w-[281px] h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+                className={"w-[273px] 2xl:w-[281px] h-[110px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
               >
-                <span className="text-[20px] font-[900] absolute top-[15px] left-[15px]">1</span>
+                <span className="text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">1</span>
                 <div className="text-[18px] font-[500] text-center whitespace-pre-line ">{text[0].title}</div>
               </div>
               {/* 2 item */}
               <div 
-                className={"w-[403px] h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+                className={"w-[331px] 2xl:w-[403px] h-[110px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
               >
-                <span className="text-[20px] font-[900] absolute top-[15px] left-[15px]">2</span>
+                <span className="text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">2</span>
                 <div className="text-[18px] font-[500] text-center whitespace-pre-line ">{text[1].title}</div>
               </div>
             </div>
             {/* 2 row */}
-            <div className="flex items-center gap-[10px] mt-[10px]">
+            <div className="hidden sm:flex items-center gap-[10px] mt-[10px]">
               {/* 1 item */}
               <div 
-                className={"w-[267px] h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+                className={"w-[230px] 2xl:w-[267px] h-[110px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
               >
-                <span className="text-[20px] font-[900] absolute top-[15px] left-[15px]">3</span>
+                <span className="text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">3</span>
                 <div className="text-[18px] font-[500] text-center whitespace-pre-line ">{text[2].title}</div>
               </div>
               {/* 2 item */}
               <div 
-                className={"w-[170px] h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+                className={"w-[140px] 2xl:w-[170px] h-[110px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
               >
-                <span className="text-[20px] font-[900] absolute top-[15px] left-[15px]">4</span>
+                <span className="text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">4</span>
                 <div className="text-[18px] font-[500] text-center whitespace-pre-line ">{text[3].title}</div>
               </div>
               {/* 3 item */}
               <div 
-                className={"w-[237px] h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+                className={"w-[225px] 2xl:w-[237px] h-[110px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
               >
-                <span className="text-[20px] font-[900] absolute top-[15px] left-[15px]">5</span>
+                <span className="text-[18px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">5</span>
                 <div className="text-[18px] font-[500] text-center whitespace-pre-line ">{text[4].title}</div>
               </div>
             </div>
+
+            {/* On Mobile */}
+            {/* 1 row */}
+            <div className="flex sm:hidden  items-center gap-[10px]">
+              {/* 1 item */}
+              <div 
+                className={"w-[120px] 2xl:w-[281px] h-[90px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+              >
+                <span className="text-[10px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">1</span>
+                <div className="text-[10px] font-[500] text-center whitespace-pre-line ">{text[0].title}</div>
+              </div>
+              {/* 2 item */}
+              <div 
+                className={"w-[93px] 2xl:w-[403px] h-[90px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+              >
+                <span className="text-[10px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">2</span>
+                <div className="text-[10px] font-[500] text-center whitespace-pre-line ">{text[1].title}</div>
+              </div>
+              {/* 3 item */}
+              <div 
+                className={"w-[146px] 2xl:w-[403px] h-[90px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+              >
+                <span className="text-[10px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">3</span>
+                <div className="text-[10px] font-[500] text-center whitespace-pre-line ">{text[1].title}</div>
+              </div>
+            </div>
+            {/* 2 row */}
+            <div className="flex sm:hidden items-center gap-[10px] mt-[10px]">
+              {/* 1 item */}
+              <div 
+                className={"w-[172px] 2xl:w-[267px] h-[90px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+              >
+                <span className="text-[10px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">3</span>
+                <div className="text-[10px] font-[500] text-center whitespace-pre-line ">{text[2].title}</div>
+              </div>
+              {/* 2 item */}
+              <div 
+                className={"w-[198px] 2xl:w-[170px] h-[90px] 2xl:h-[150px] rounded-[20px] flex items-center justify-center relative bg-[#E8E7E7] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]"}
+              >
+                <span className="text-[10px] 2xl:text-[20px] font-[900] absolute top-[10px] 2xl:top-[15px] left-[10px] 2xl:left-[15px]">4</span>
+                <div className="text-[10px] font-[500] text-center whitespace-pre-line ">{text[3].title}</div>
+              </div>
+            </div>
+            <button className="inline-flex sm:hidden w-full 2xl:w-[350px] h-[50px] 2xl:h-[75px] items-center justify-center bg-[#333] text-[#fff] text-[14px] 2xl:text-[24px] rounded-[20px] mt-[15px]">Консультация</button>
           </div>
         </div>
       </div>
